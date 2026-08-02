5e1bebd5ee41e4a1b7086a16be71775dc816da2f
jamesgreen-c <jamesenricodavidgreen@gmail.com>
2026-08-02 23:17:40 +0100

    Rework BayesianInference for memory efficient sample storage

--- rbsmc/bayesian/training_rework.py
+++ rbsmc/bayesian/training_rework.py
@@ -35,7 +35,7 @@ class Config:
 
     debug: bool = False
 
-
+# TODO rename to Particle Gibbs
 class BayesianInference:
 
     def __init__(
@@ -64,6 +64,7 @@ class BayesianInference:
             key,
             params,
             state: Array,
+            dts: Array,
             data: Union[Array, Tuple[Array]],
         ):
         """
@@ -71,11 +72,10 @@ class BayesianInference:
 
         Parameters
         ----------
-        key:         PRNG key used by the stochastic free-energy estimator.
-        params:      Dictionary with fields:
-                        - "prior": prior parameters with trainable grad/exact blocks and fixed parameters.
-        samples:     Cached reference paths for the current minibatch.
-        data:        Mini-batch of observations.
+        key:      RNG.
+        params:   Dictionary containing parameters for prior model.
+        samples:  Cached reference path.
+        data:     Observations.
 
         Returns
         -------
@@ -87,118 +87,215 @@ class BayesianInference:
         energy = 0 # how and where to put energy calculation 
 
         # M step
-        new_params = self.gibbs.update(key_m, params, state[0])
+        new_params = self.gibbs.update(key_m, params, state[0], dts, data)
         return energy, new_params, state, aux
 
 
-    def run(self, data):
+    def run(self, data, dts: Array, hyperparams: dict):
         """
         Runs Bayesian Inference.
 
         Parameters
         ----------
-        data:  PyTree of observations with leaves of shape (N, T, *_), where 
-                - N is the number of independent time-series
-                - T is the number of time-steps
-
-        Returns
-        -------
-        best_params: Parameter dictionary achieving the lowest observed stochastic loss.
+        data:         PyTree of observations with leaves of shape (T, *D), where 
+                          - T is the number of time-steps
+                          - *D is the arbitrary observation dimension
+        dts:          The set of time-differentials between each observation time
+        hyperparams:  Fixed / initial parameters required for prior initialisation
         """
-       
         data_leaf = tree_util.tree_leaves(data)[0]
-        N, T = data_leaf.shape[:2]
+        T = data_leaf.shape[0]
 
         train_step = jax.jit(self.train_step) if not self.config.debug else self.train_step
         
         # initialisation
         key, sample_key, param_key = jr.split(jr.PRNGKey(self.config.seed), 3)
-        self.params = self.gibbs.init(param_key)
+        self.params = self.gibbs.init(param_key, hyperparams)
         state = self.smc.init(sample_key, self.params, data)
 
-        # stores
-        self.energies = []
-        self.replaced_hist = jnp.zeros((T, self.config.replacement_rate_window)) * jnp.nan  
-        self.replacement_rates = []
-
-        self.param_hist = deepcopy(self.params)
-        self.sample_hist = tree_util.tree_map(lambda _s: _s[None, ...], state[0])  # prepend itr dimension
-        self.ancestor_hist = state[1][None, ...]                                   # prepend itr dimension
+        # memory efficient stores
+        self.energies = np.empty(self.config.burnin + self.config.samples, dtype=np.float32)
+        self._allocate_hist(state, self.params, T)
 
         # run
         pbar = tqdm(range(self.config.burnin + self.config.samples))
-        for self.itr in pbar:
+        for itr in pbar:
             key, subkey = jr.split(key)
 
-            energy, self.params, state, aux = train_step(subkey, self.params, state, data)
+            energy, self.params, state, aux = train_step(subkey, self.params, state, dts, data)
 
             # track energy
             energy_float = float(energy)
-            self.energies.append(energy)
+            self.energies[itr] = energy_float
             pbar.set_postfix(loss=f"{energy_float:.3f}")
 
-            # store parameter history for traces
-            self.param_hist = self._append_param_hist(self.itr, self.param_hist, self.params)
-            
-            # store trajectory and ancestory history
-            self.sample_hist = self._append_state_hist(self.sample_hist, state[0])
-            self.ancestor_hist = self._append_state_hist(self.ancestor_hist, state[1])
+            replacement_rates = self._calculate_replacement_rate(aux["replaced"])
+            self._store_iteration(itr, state, self.params, replacement_rates)
 
-            # track replacement rate
-            replacement_rates = self._calculate_replacement_rate(aux)
-            self.replacement_rates.append(replacement_rates)
+        return self.sample_hist, self.ancestor_hist, self.param_hist, self.replacement_rates
 
-        return self.sample_hist, self.ancestor_hist, self.param_hist, self.replacement_hist
+    def _allocate_hist(self, state, params, T):
+        total = self.config.burnin + self.config.samples
 
-    def _calculate_replacement_rate(self, aux: dict):
-        """
-        Calculate the replacement rate of SMC kernel over a window of sample time
-        """
-        replaced = aux["replaced"]             # (T,)
+        # includes the initial draw at index 0
+        self.sample_hist = tree_util.tree_map(lambda x: np.empty((total + 1,) + x.shape), state[0])
+        self.ancestor_hist = np.empty((total + 1,) + state[1].shape)
+        self.param_hist = tree_util.tree_map(lambda x: np.empty((total + 1,) + x.shape), params)
+        self.replacement_rates = np.empty((total, T), dtype=np.float32)
 
-        # maintain a replacement rate window
-        self.replaced_hist = self.replaced_hist.at[:, 1:].set(self.replaced_hist[:, :-1])  # (T, Window)
-        self.replaced_hist = self.replaced_hist.at[:, 0].set(replaced)                     # (T, Window)
+        # store initial samples 
+        self.sample_hist = tree_util.tree_map(
+            lambda hist, x: self._set_host_value(hist, 0, x),
+            self.sample_hist,
+            state[0],
+        )
+        self.param_hist = tree_util.tree_map(
+            lambda hist, x: self._set_host_value(hist, 0, x),
+            self.param_hist,
+            params,
+        )
+        self.ancestor_hist[0] = np.asarray(state[1])
 
-        # replacement rate calculated over said window
-        replacement_rates = jnp.nanmean(self.replaced_hist, 1) # (T,)
-        return replacement_rates
-    
-    def _append_param_hist(self, itr: int, hist: dict, params: dict):
-        """
-        Appends the current trainable prior parameters along a leading
-        iteration axis while leaving fixed parameters unchanged.
-        """
-        hist_trainable = hist["prior"]["trainable"]
-        trainable = params["prior"]["trainable"]
+        # replacement rate window
+        window = min(self.config.replacement_rate_window, total)
+        self.replaced_hist = np.zeros((window, T), dtype=bool)
+        self.replaced_count = 0
+        self.replaced_position = 0
 
-        # append the latest value to the existing history axis
-        if itr == 0:
-            hist_trainable = tree_util.tree_map(
-                lambda old, new: jnp.stack((old, new), axis=0),
-                hist_trainable,
-                trainable,
-            )
-        else:
-            hist_trainable = tree_util.tree_map(
-                lambda old, new: jnp.concatenate((old, new[None, ...]), axis=0),
-                hist_trainable,
-                trainable,
-            )
+    def _store_iteration(self, itr, state, params, replacement_rates):
+        """ index 0 is initialisation; Gibbs iteration itr is stored at itr + 1 """
+        hist_idx = itr + 1
 
-        return {**hist, "prior": {**hist["prior"], "trainable": hist_trainable,}}
-    
-    def _append_state_hist(self, hist, sample):
-        """
+        self.sample_hist = tree_util.tree_map(
+            lambda hist, x: self._set_host_value(hist, hist_idx, x),
+            self.sample_hist,
+            state[0],
+        )
+        self.param_hist = tree_util.tree_map(
+            lambda hist, x: self._set_host_value(hist, hist_idx, x),
+            self.param_hist,
+            params,
+        )
+        self.ancestor_hist[hist_idx] = np.asarray(state[1])
+        self.replacement_rates[itr] = replacement_rates
+
+    @staticmethod
+    def _set_host_value(hist, idx, value):
+        hist[idx] = np.asarray(value)
+        return hist
+
+    def _calculate_replacement_rate(self, replaced):
+        """ Calculate the replacement rate of SMC kernel over a window of sample time """
+        replaced = np.asarray(replaced, dtype=bool)
+
+        self.replaced_hist[self.replaced_position] = replaced
+        self.replaced_position = (self.replaced_position + 1) % self.replaced_hist.shape[0]
+        self.replaced_count = min(self.replaced_count + 1, self.replaced_hist.shape[0])
+
+        return self.replaced_hist[:self.replaced_count].mean(axis=0)
+
+
+
+# def run(self, data, dts: Array, hyperparams: dict):
+#         """
+#         Runs Bayesian Inference.
+
+#         Parameters
+#         ----------
+#         data:         PyTree of observations with leaves of shape (T, *D), where 
+#                           - T is the number of time-steps
+#                           - *D is the arbitrary observation dimension
+#         dts:          The set of time-differentials between each observation time
+#         hyperparams:  Fixed / initial parameters required for prior initialisation
+#         """
+       
+#         data_leaf = tree_util.tree_leaves(data)[0]
+#         T = data_leaf.shape[0]
+
+#         train_step = jax.jit(self.train_step) if not self.config.debug else self.train_step
         
-        Parameters
-        ----------
-        hist:    Pytree (itr, T, *D) - current total sample history
-        sample:  Pytree (T, *D)      - single sample from SMC with current prior params
+#         # initialisation
+#         key, sample_key, param_key = jr.split(jr.PRNGKey(self.config.seed), 3)
+#         self.params = self.gibbs.init(param_key, hyperparams)
+#         state = self.smc.init(sample_key, self.params, data)
 
-        Returns
-        -------
-        new_hist:  Extended sample history
-        """
-        return tree_util.tree_map(lambda old, new: jnp.concatenate((old, new[None, ...]), axis=0), hist, sample)
+#         # stores
+#         self.energies = []
+#         self.replaced_hist = jnp.zeros((T, self.config.replacement_rate_window)) * jnp.nan  
+#         self.replacement_rates = []
+
+#         self.param_hist = deepcopy(self.params)
+#         self.sample_hist = tree_util.tree_map(lambda _s: _s[None, ...], state[0])  # prepend itr dimension
+#         self.ancestor_hist = state[1][None, ...]                                   # prepend itr dimension
+
+#         # run
+#         pbar = tqdm(range(self.config.burnin + self.config.samples))
+#         for self.itr in pbar:
+#             key, subkey = jr.split(key)
+
+#             energy, self.params, state, aux = train_step(subkey, self.params, state, dts, data)
+
+#             # track energy
+#             energy_float = float(energy)
+#             self.energies.append(energy)
+#             pbar.set_postfix(loss=f"{energy_float:.3f}")
+
+#             # store parameter history for traces
+#             self.param_hist = self._append_param_hist(self.itr, self.param_hist, self.params)
+            
+#             # store trajectory and ancestory history
+#             self.sample_hist = self._append_state_hist(self.sample_hist, state[0])
+#             self.ancestor_hist = self._append_state_hist(self.ancestor_hist, state[1])
+
+#             # track replacement rate
+#             replacement_rates = self._calculate_replacement_rate(aux)
+#             self.replacement_rates.append(replacement_rates)
+
+#         return self.sample_hist, self.ancestor_hist, self.param_hist, self.replacement_rates
+
+    # def _calculate_replacement_rate(self, aux: dict):
+    #     """
+    #     Calculate the replacement rate of SMC kernel over a window of sample time
+    #     """
+    #     replaced = aux["replaced"]             # (T,)
+
+    #     # maintain a replacement rate window
+    #     self.replaced_hist = self.replaced_hist.at[:, 1:].set(self.replaced_hist[:, :-1])  # (T, Window)
+    #     self.replaced_hist = self.replaced_hist.at[:, 0].set(replaced)                     # (T, Window)
+
+    #     # replacement rate calculated over said window
+    #     replacement_rates = jnp.nanmean(self.replaced_hist, 1) # (T,)
+    #     return replacement_rates
+
+    # def _append_param_hist(self, itr: int, hist: dict, params: dict):
+    #     """ 
+    #     Appends the current trainable prior parameters along a leading iteration axis
+
+    #     Parameters
+    #     ----------
+    #     itr:     current iteration number
+    #     hist:    dictionary of historical parameter values
+    #     params:  dictionary of current iteration parameter values
+        
+    #     Returns
+    #     -------
+    #     updated_hist 
+    #     """
+    #     if itr == 0:
+    #         return tree_util.tree_map(lambda old, new: jnp.stack((old, new)), hist, params)
+    #     return tree_util.tree_map(lambda old, new: jnp.concatenate((old, new[None]), axis=0), hist, params)
+    
+    # def _append_state_hist(self, hist, sample):
+    #     """
+        
+    #     Parameters
+    #     ----------
+    #     hist:    Pytree (itr, T, *D) - current total sample history
+    #     sample:  Pytree (T, *D)      - single sample from SMC with current prior params
+
+    #     Returns
+    #     -------
+    #     new_hist:  Extended sample history
+    #     """
+    #     return tree_util.tree_map(lambda old, new: jnp.concatenate((old, new[None, ...]), axis=0), hist, sample)
     
